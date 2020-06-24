eb80eb505023ad3bc345a20b58705683eb47f76d
Alex Forshtat <forshtat1@gmail.com>
2020-06-24 04:45:47 +0300

    Add prepare script (#6)
    * Add 'prepare' script to copy compiled json to src folder
    * Cannot call it 'prepare' until we use GSN form NPM
    * Avoid dependency on global web3

--- src/ProxyRelayProvider.ts
+++ src/ProxyRelayProvider.ts
@@ -6,13 +6,14 @@ import { Address } from '@opengsn/gsn/dist/src/relayclient/types/Aliases'
 
 import { HttpProvider } from 'web3-core'
 import { JsonRpcPayload } from 'web3-core-helpers'
+import Contract from 'web3-eth-contract'
 import { keccak256 } from 'web3-utils'
 import abi from 'web3-eth-abi'
 import { removeHexPrefix } from '@opengsn/gsn/dist/src/common/Utils'
 
-export default class ProxyRelayProvider extends RelayProvider {
-  private readonly proxyIdentityArtifact = require('../build/contracts/ProxyIdentity.json')
+import ProxyIdentityArtifact from './compiled/ProxyIdentity.json'
 
+export default class ProxyRelayProvider extends RelayProvider {
   private readonly proxyFactory: Address
 
   constructor (
@@ -31,16 +32,18 @@ export default class ProxyRelayProvider extends RelayProvider {
   _ethSendTransaction (payload: JsonRpcPayload, callback: JsonRpcCallback): void {
     const gsnTransactionDetails: GsnTransactionDetails = payload.params[0]
     const proxyAddress = this._calculateProxyAddress(gsnTransactionDetails.from, 0)
-    const proxy = new web3.eth.Contract(this.proxyIdentityArtifact.abi, proxyAddress)
+    // @ts-ignore
+    const proxy = new Contract(ProxyIdentityArtifact.abi, proxyAddress)
     payload.params[0].data = proxy.methods.execute(0, gsnTransactionDetails.to, 0, gsnTransactionDetails.data).encodeABI()
     payload.params[0].to = proxyAddress
     super._ethSendTransaction(payload, callback)
   }
 
+  // @ts-ignore
   _calculateProxyAddress (owner: Address, salt: number): Address {
     const ff = '0xff'
     const deployingAddress = removeHexPrefix(this.proxyFactory).toLowerCase()
-    const bytecode = this.proxyIdentityArtifact.bytecode
+    const bytecode = ProxyIdentityArtifact.bytecode
     // @ts-ignore
     const constructorParameters = removeHexPrefix(abi.encodeParameters(['address'], [owner])).toLowerCase()
     const initCode = bytecode.concat(constructorParameters).toLowerCase()
